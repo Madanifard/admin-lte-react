ce3f0f9d4cd4f8afb183cc207de15744642e63dd
Amirreza Madanifard <amirreza@debian.Amirreza.Madanifard>
2023-12-30 00:41:51 +0330

    add Fontawesome

--- src/Views/Layouts/Components/DashLayoutComponents/LeftNavbar.jsx
+++ src/Views/Layouts/Components/DashLayoutComponents/LeftNavbar.jsx
@@ -1,9 +1,17 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  fas,
+  faBars
+} from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from 'react';
+
+
 const LeftNavbar = () => {
   return (
     <ul className="navbar-nav">
       <li className="nav-item">
-        <a className="nav-link" data-widget="pushmenu" href="#" role="button">
-          <i className="fas fa-bars"></i>
+        <a className="nav-link" href="#" role="button">
+          <FontAwesomeIcon icon={(fas, faBars)} />
         </a>
       </li>
       <li className="nav-item d-none d-sm-inline-block">
